65294616949ca21dac28314b44dcb6c5a12c28b8
Thalyson <thalysonbarrospereira2019@gmail.com>
2022-04-11 20:22:46 -0300

    Ajuste de indentação

--- App/Model/Usuario.php
+++ App/Model/Usuario.php
@@ -1,50 +1,50 @@
 <?php
 
-class Usuario {
+    class Usuario {
 
-    public function validaLogin($email, $senha) {
+        public function validaLogin($email, $senha) {
 
-        global $pdo;
+            global $pdo;
 
-        $sql = "SELECT * FROM tb_usuario WHERE email_usuario = :email_usuario AND pass_usuario = MD5(:pass_usuario);";
-        $sql = $pdo->prepare($sql);
-        $sql->bindValue('email_usuario', $email);
-        $sql->bindValue('pass_usuario', $senha);
-        $sql->execute();
+            $sql = "SELECT * FROM tb_usuario WHERE email_usuario = :email_usuario AND pass_usuario = MD5(:pass_usuario);";
+            $sql = $pdo->prepare($sql);
+            $sql->bindValue('email_usuario', $email);
+            $sql->bindValue('pass_usuario', $senha);
+            $sql->execute();
 
-        if ($sql->rowCount() > 0) {
+            if ($sql->rowCount() > 0) {
 
-            $dado = $sql->fetch();
-            $_SESSION['idUserSession'] = $dado['id_usuario'];
-            return TRUE;
+                $dado = $sql->fetch();
+                $_SESSION['idUserSession'] = $dado['id_usuario'];
+                return TRUE;
 
-        } else {
+            } else {
 
-            return FALSE;
+                return FALSE;
+
+            }
+
+        }
+
+        public function usuarioLogado($id) {
+
+            global $pdo;
+
+            $array = array();
+
+            $sql = "SELECT nome_usuario FROM tb_usuario WHERE id_usuario = :id_usuario;";
+            $sql = $pdo->prepare($sql);
+            $sql->bindValue('id_usuario', $id);
+            $sql->execute();
+
+            if ($sql->rowCount() > 0) {
+
+                $array = $sql->fetch();
+
+            } 
+
+            return $array;
 
         }
 
     }
-
-    public function usuarioLogado($id) {
-
-        global $pdo;
-
-        $array = array();
-
-        $sql = "SELECT nome_usuario FROM tb_usuario WHERE id_usuario = :id_usuario;";
-        $sql = $pdo->prepare($sql);
-        $sql->bindValue('id_usuario', $id);
-        $sql->execute();
-
-        if ($sql->rowCount() > 0) {
-
-            $array = $sql->fetch();
-
-        } 
-
-        return $array;
-
-    }
-
-}
