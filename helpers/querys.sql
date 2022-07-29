59a3acb8519ef911f15e4badc21d9a8271954ff9
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2022-07-29 20:06:19 -0300

    CHORE: Estruturando banco de dados

---
+++ helpers/querys.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE db_boxcontrol;
+
+USE db_boxcontrol;
+
+CREATE TABLE tb_user (
+    id SMALLINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    code_user INT(10) NOT NULL,
+    name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    date_change DATETIME
+);
+
+CREATE TABLE tb_log (
+	id SMALLINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	email VARCHAR(250) NOT NULL,
+	action VARCHAR(250) NOT NULL,
+	date_inclusion DATETIME
+);
+
+CREATE TABLE tb_titlereceived (
+    id SMALLINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    codetitle INT(10) NOT NULL,
+    description VARCHAR(250) NOT NULL,
+    value VARCHAR(250) NOT NULL,
+    date_inclusion DATETIME NOT NULL,
+    code_user INT(10) NOT NULL
+);
+
+CREATE TABLE tb_titlepaid (
+    id SMALLINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    codetitle INT(10) NOT NULL,
+    description VARCHAR(250) NOT NULL,
+    value VARCHAR(250) NOT NULL,
+    date_inclusion DATETIME NOT NULL,
+    code_user INT(10) NOT NULL
+);
+
+CREATE TABLE tb_increment (
+    id SMALLINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    codefunction VARCHAR(2) NOT NULL,
+    sequence INT(10) NOT NULL,
+    date_change DATETIME
+);
